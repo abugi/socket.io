3de2d50e832a3ebce14e43d02dea4185f1079687
abugi <abugiboi@gmail.com>
2017-12-15 20:26:29 +0100

    initial commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var  express = require('express'),
      app = express(),
      server = require('http').createServer(app),
      io = require('socket.io').listen(server);
+     usernames = [];
 
      server. listen(process.env.PORT || 3000, function(){console.log("Chat app running live on port 3000")});
 
@@ -11,6 +12,7 @@ var  express = require('express'),
 
      //connect to scocket.io
      io.sockets.on('connection', function(socket){
+
           //send message
           socket.on('send message', function(data){
                io.sockets.emit('new message', {msg: data});
